7d3d4ee83a392bceca9cb00d0759a9d206c6879b
celianvdb <celian@protonmail.com>
2018-11-16 23:06:19 +0100

    oups..forgot to commit

--- app/middlewares/auth/isLogged.js
+++ app/middlewares/auth/isLogged.js
@@ -4,15 +4,21 @@ module.exports = async function(req, res, next) {
 		return res.errors(['No token provided.']);
 
 	var token = req.query.token;
-	
+
+	//Decode the token
 	token = await Token.getTokenFromJWT(token);
 	if(!token) {
 		return res.errors(['An error occured.']);
 	}
 
+	//Check if the user still have an auth provider linked to his account
+	
+
+	//Check if it's an access token
 	if(token.type)
 		return res.errors(['Please use your access token.']);
 
+	//Check the status of the token
 	switch(token.tokenStatus()){
 		case 0:
 			res.errors(['Invalid or disabled token.']);
