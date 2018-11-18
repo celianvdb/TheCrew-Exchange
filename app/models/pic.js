09037aaf278c7df6b67b1c9d602ca3baa6ff1db6
celianvdb <celian@protonmail.com>
2018-11-18 01:43:31 +0100

    Beginning to work on the upload system

--- app/models/pic.js
+++ app/models/pic.js
@@ -1,14 +1,16 @@
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId
+const config = require('../config.json');
 var fs = require('fs');
-const config = require('../../config.json');
 
 var Pic = new Schema({
 	identifier : { type: String },
 	user_id: { type: ObjectId },
 	librarys : { type: Array },
 	uploaded_date: { type: Date, default: Date.now },
-	checksum : { type: String },
-	filesize : { type: Number },
+	checksum : { type: String }, //md5 Checksum of the original file only
+	filesize : { type: Number }, //Filesize of the original file only (Bytes)
 	showable : { type: Boolean, default: true }
 });
 
@@ -16,5 +18,27 @@ Pic.methods.getFile = function() {
 	var path = config.storage.pictures + this.identifier;
 	this.file = fs.readFileSync(path);
 };
+
+Pic.statics.createIdentifier = async function() {
+	function genIdentifier() {
+		
+		$characters = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		$string = '';
+		
+		for ($i = 0; $i < 8; $i++) {
+				$string += $characters[Math.floor((Math.random() * $characters.length) + 0)];
+		}
+		return $string;
+
+	}
+
+	var identifier = genIdentifier();
+
+	while(await this.findOne({ 'identifier' : identifier })) {
+		identifier = genIdentifier(); 
+	}
+
+	return identifier;
+}
 
 module.exports = mongoose.model('Pic', Pic);
--- app/router/pics/index.js
+++ app/router/pics/index.js
@@ -1,10 +1,46 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
+const bodyParser = require('body-parser');
 const router = require('express-promise-router')();
 const config = require('../../config.json');
 const isLogged = require('../../middlewares/auth/isLogged.js');
+const fs = require('fs');
+const Pic = require('../../models/pic.js');
 
-router.post('/upload', /*isLogged, */(req , res, next) => { //isLogged ByPass temporary for dev reasons
-	console.log(req.params);
+router.use(fileUpload());
+router.use(bodyParser.json());
+
+router.post('/upload', /*isLogged, */async (req , res, next) => { //isLogged ByPass temporary for dev reasons
+	if(req.files && req.body.librarys) { // files an librarys are required
+		
+		if(req.files.picture) { // Image process
+			if(req.files.picture.mimetype == 'image/jpeg' || req.files.picture.mimetype == 'image/png') {
+				/*TODO
+					Check if library exist
+					Check size of pic
+					Check for stupid ratio ?
+				*/
+				var identifier = await Pic.createIdentifier();
+	
+				var folder = config.storage.pictures + identifier;
+				if (!fs.existsSync(folder)) {
+					fs.mkdirSync(folder);
+				}
+	
+				fs.writeFileSync(folder + '/' + identifier, req.files.picture.data);
+				var pic = new Pic({filesize : req.files.picture.data.length, 'identifier' : identifier , 'checksum' : req.files.picture.md5()}).save();
+				console.log(folder + '/' + identifier, identifier)
+			} else {
+				res.errors(['Unallowed file type, need to be png or jpeg.']);
+			}
+		}else if(req.files.video) { // Video process 
+			// Soon™
+			res.errors(['This feature is not ready for now.']);
+		}else {
+			res.errors(['No picture or video sent.']);
+		}
+
+	}
 });
 
 module.exports = router;
